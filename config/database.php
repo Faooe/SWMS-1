cf84c639afb386506ad840e293b3217f2c0e47cc
Faooe <nfarinn1@gmail.com>
2026-07-15 14:15:54 +0800

    fix: update deprecated pdo constant for php 8.5

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 use Pdo\Mysql;
+use Pdo\Pgsql; // Impor class Pgsql untuk PHP 8.5+
 
 return [
 
@@ -85,23 +86,23 @@ return [
         ],
 
         'pgsql' => [
-        'driver' => 'pgsql',
-        'host' => env('DB_HOST', '127.0.0.1'),
-        'port' => env('DB_PORT', '5432'),
-        'database' => env('DB_DATABASE', 'forge'),
-        'username' => env('DB_USERNAME', 'forge'),
-        'password' => env('DB_PASSWORD', ''),
-        'charset' => 'utf8',
-        'prefix' => '',
-        'prefix_indexes' => true,
-        'search_path' => 'public',
-        'sslmode' => env('DB_SSLMODE', 'prefer'),
-        // Tambahkan baris di bawah ini:
-        'options' => [
-            PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
-            '--project' => 'ep-patient-tooth-at11k00y-pooler',
+            'driver' => 'pgsql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+                Pgsql::ATTR_DISABLE_PREPARES => true, // Menggunakan standar PHP 8.5 modern
+                '--project' => 'ep-wispy-queen-ates8kxc',
+            ],
         ],
-    ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
