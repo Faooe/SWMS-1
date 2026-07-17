f5695ca1d3dc9b031c084f3a55ff7d0139ef88e5
Faooe <nfarinn1@gmail.com>
2026-07-17 12:01:15 +0800

    mata password

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -7,7 +7,12 @@
     'required' => false,
 ])
 
-<div class="space-y-2">
+@php
+    // Toggle mata (show/hide) otomatis aktif untuk field bertipe password
+    $isPassword = $type === 'password';
+@endphp
+
+<div class="space-y-2" @if($isPassword) x-data="{ showPassword: false }" @endif>
 
     @if($label)
 
@@ -27,58 +32,83 @@
 
     @endif
 
-    <input
+    <div class="relative">
 
-        @if($name)
-            id="{{ $name }}"
-            name="{{ $name }}"
+        <input
+
+            @if($name)
+                id="{{ $name }}"
+                name="{{ $name }}"
+            @endif
+
+            @if($isPassword)
+                :type="showPassword ? 'text' : 'password'"
+            @else
+                type="{{ $type }}"
+            @endif
+
+            value="{{ old($name ?? '', $value) }}"
+
+            placeholder="{{ $placeholder }}"
+
+            @if($required)
+                required
+            @endif
+
+            {{ $attributes->merge([
+
+                'class' => '
+                    w-full
+                    rounded-2xl
+                    border
+                    border-slate-300
+                    bg-white
+                    px-4
+                    py-3
+                    text-slate-800
+                    shadow-sm
+                    outline-none
+                    transition
+                    duration-200
+
+                    placeholder:text-slate-400
+
+                    focus:border-blue-500
+                    focus:ring-4
+                    focus:ring-blue-100
+
+                    disabled:bg-slate-100
+                    disabled:text-slate-500
+                    disabled:cursor-not-allowed
+
+                    '.($isPassword ? 'pr-12' : '').'
+
+                    '.(
+                        $name && $errors->has($name)
+                            ? 'border-red-500 focus:border-red-500 focus:ring-red-100'
+                            : ''
+                    )
+
+            ]) }}
+
+        >
+
+        @if($isPassword)
+
+            <button
+                type="button"
+                @click="showPassword = !showPassword"
+                tabindex="-1"
+                class="absolute inset-y-0 right-0 flex items-center pr-4 text-slate-400 transition hover:text-slate-600">
+
+                <span x-show="!showPassword"><i data-lucide="eye" class="h-5 w-5"></i></span>
+                <span x-show="showPassword" style="display: none;"><i data-lucide="eye-off" class="h-5 w-5"></i></span>
+
+            </button>
+
         @endif
 
-        type="{{ $type }}"
-
-        value="{{ old($name ?? '', $value) }}"
-
-        placeholder="{{ $placeholder }}"
-
-        @if($required)
-            required
-        @endif
-
-        {{ $attributes->merge([
-
-            'class' => '
-                w-full
-                rounded-2xl
-                border
-                border-slate-300
-                bg-white
-                px-4
-                py-3
-                text-slate-800
-                shadow-sm
-                outline-none
-                transition
-                duration-200
-
-                placeholder:text-slate-400
-
-                focus:border-blue-500
-                focus:ring-4
-                focus:ring-blue-100
-
-                disabled:bg-slate-100
-                disabled:text-slate-500
-                disabled:cursor-not-allowed
-
-                '.(
-                    $name && $errors->has($name)
-                        ? 'border-red-500 focus:border-red-500 focus:ring-red-100'
-                        : ''
-                )
-
-        ]) }}
-
-    >
+    </div>
 
     @if($name)
 
